69c1def9c0103d4593ed7f4d891ca03ba5d961b2
nimeshaviduranga <49368109+nimeshaviduranga@users.noreply.github.com>
2025-06-07 11:35:48 +0530

    Handle Auth service logic

---
+++ src/main/java/com/eventmanagement/service/AuthService.java
@@ -0,0 +1,79 @@
+package com.eventmanagement.service;
+
+import com.eventmanagement.dto.auth.AuthResponse;
+import com.eventmanagement.dto.auth.LoginRequest;
+import com.eventmanagement.dto.auth.RegisterRequest;
+import com.eventmanagement.entity.User;
+import com.eventmanagement.repository.UserRepository;
+import com.eventmanagement.security.CustomUserDetails;
+import com.eventmanagement.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     *Handle User Registration
+     */
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email is already registered");
+        }
+
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()),
+                request.getRole()
+        );
+
+        User savedUser = userRepository.save(user);
+        CustomUserDetails userDetails = new CustomUserDetails(savedUser);
+        String token = jwtUtil.generateToken(userDetails);
+
+        return createAuthResponse(token, savedUser);
+    }
+
+    /**
+     * Handle User login
+     */
+    public AuthResponse login(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String token = jwtUtil.generateToken(userDetails);
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not fount with the email"));
+
+        return createAuthResponse(token, user);
+    }
+
+
+    private AuthResponse createAuthResponse(String token, User user) {
+        AuthResponse.UserInfo userInfo = new AuthResponse.UserInfo(
+                user.getId().toString(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole().name()
+        );
+
+        return new AuthResponse(token, userInfo);
+    }
+}
